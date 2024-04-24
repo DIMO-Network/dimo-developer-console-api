875a823ff08e0978a0afefbab900a0e58f7347f6
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-04-23 23:09:41 -0500

    Added endpoints to get the current user information

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,4 +1,5 @@
 import Controller from '@/controllers/auth.controller';
+import { headers } from 'next/headers';
 
 interface IProps {
   code: string;
@@ -18,9 +19,29 @@ export async function POST(request: Request) {
   } catch (error: any) {
     console.error({
       error,
-      step: 'OAuth process',
+      step: '[OAuth] Sign In/Up process',
       code,
       app,
+    });
+    return Response.json(
+      {
+        message: error.message,
+      },
+      { status: 400 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  const token = headers().get('Authorization') ?? '';
+
+  try {
+    const user = await Controller.getUserByToken(token);
+    return Response.json(user);
+  } catch (error: any) {
+    console.error({
+      error,
+      step: '[OAuth] Get user information by token',
     });
     return Response.json(
       {
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import AuthService from '@/services/auth';
 import AuthUtils from '@/utils/auth';
 import { User } from '@/models/user.model';
+import { JwtPayload } from 'jsonwebtoken';
 
 export const processOAuth = async (code: string, app: string) => {
   const { token, user } = await AuthService.processOAuth(code, app);
@@ -24,7 +25,13 @@ export const generateToken = ({ id = '' }: User) => {
   return AuthUtils.generateToken({ id });
 };
 
+export const getUserByToken = async (token: string) => {
+  const { id: userId } = AuthUtils.verifyAuth(token) as JwtPayload;
+  return User.findOne({ where: { id: userId } });
+};
+
 export default {
   processOAuth,
   generateToken,
+  getUserByToken,
 };
