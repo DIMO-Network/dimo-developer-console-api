eec97730d682833c87e09ec80f8def3024e177a5
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2024-05-28 22:20:41 -0500

    Added Company model

---
+++ src/models/company.model.ts
@@ -0,0 +1,120 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+  Sequelize,
+} from 'sequelize';
+
+import DB from '@/services/db';
+import { PaginationOptions, paginateData } from '@/utils/paginateData';
+import { FilterObject, transformObjectToSequelize } from '@/utils/filter';
+
+export class Company extends Model<
+  InferAttributes<Company>,
+  InferCreationAttributes<Company>
+> {
+  declare id?: string;
+  declare name: string;
+  declare website: string;
+  declare region: string;
+  declare type: string;
+  declare build_for: string;
+  declare build_for_text?: string;
+  declare crm_id?: string;
+  declare created_by: string;
+  declare deleted?: boolean;
+  declare deleted_at?: Date;
+
+  static findAllPaginated(
+    findOptions: FilterObject,
+    paginationOptions: PaginationOptions
+  ) {
+    const filter = transformObjectToSequelize(findOptions, {
+      like: ['name', 'website', 'build_for_text'],
+      exact: ['region', 'type', 'build_for'],
+    });
+    return paginateData(Company, { where: filter }, paginationOptions);
+  }
+}
+
+Company.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+    },
+    build_for: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+      },
+    },
+    build_for_text: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    crm_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    created_by: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true,
+      },
+    },
+    deleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize: DB.connection as Sequelize,
+    modelName: 'Company',
+    tableName: 'companies',
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  }
+);
