10701cbdac289403d7d47effb987b3774c6b9e92
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2024-11-07 19:59:36 +0100

    [fix] change missing .xyz to .org domains

--- src/config/preview.ts
+++ src/config/preview.ts
@@ -1,3 +1,3 @@
-export const backendUrl = 'https://console-staging-api.dimo.xyz/';
+export const backendUrl = 'https://console-staging-api.dimo.org/';
 
-export const frontendUrl = 'https://console-staging.dimo.xyz/';
+export const frontendUrl = 'https://console-staging.dimo.org/';
