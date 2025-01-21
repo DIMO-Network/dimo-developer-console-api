03e25343e96298018cb7eae8b54e47f031ed2a43
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2025-01-21 15:27:14 +0100

    remove provider validation

--- src/app/api/auth/exist/route.ts
+++ src/app/api/auth/exist/route.ts
@@ -3,11 +3,9 @@ import { findUserByEmailOrAddress } from '@/controllers/user.controller';
 export const GET = async (request: NextRequest) => {
   const { searchParams } = request.nextUrl;
   const item = searchParams.get('item') ?? null;
-  const provider = searchParams.get('provider') ?? null;
 
-  const user = await findUserByEmailOrAddress(item, provider);
+  const user = await findUserByEmailOrAddress(item);
   return Response.json({
     existItem: !!user,
-    existAssociation: user?.auth === provider,
   });
 };
