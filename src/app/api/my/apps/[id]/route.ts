3cbd22d9aee2fa3736786355da29224507d45b07
Santiago Botero Ruiz <boterosantiago6@gmail.com>
2025-03-03 08:03:16 -0500

    feat: add update functionality for user apps

--- src/app/api/my/apps/[id]/route.ts
+++ src/app/api/my/apps/[id]/route.ts
@@ -1,5 +1,9 @@
 import { AuthenticationMiddleware } from '@/middlewares/authentication.middleware';
-import { deleteOwnApp, findMyApp } from '@/controllers/app.controller';
+import {
+  deleteOwnApp,
+  findMyApp,
+  updateMyApp,
+} from '@/controllers/app.controller';
 import { getCompanyAndTeam } from '@/controllers/user.controller';
 import { isErrorWithMessage } from '@/utils/error.utils';
 import { User } from '@/models/user.model';
@@ -47,6 +51,30 @@ export const DELETE = async (
     console.error({
       error,
       step: '[My App] Delete app',
+    });
+    const message = isErrorWithMessage(error) ? error?.message : '';
+    return Response.json({ message }, { status: 400 });
+  }
+};
+
+export const PUT = async (
+  request: NextRequest,
+  { params: { id: appId } }: Params
+) => {
+  try {
+    await AuthenticationMiddleware(request);
+    const loggedUser = request.user?.user as User;
+    const userCompleteInfo = await getCompanyAndTeam(loggedUser);
+    const companyId = userCompleteInfo?.company?.id ?? '';
+
+    const newData = await request.json();
+    await updateMyApp(appId, companyId, newData);
+
+    return Response.json({});
+  } catch (error: unknown) {
+    console.error({
+      error,
+      step: '[My App] Update app',
     });
     const message = isErrorWithMessage(error) ? error?.message : '';
     return Response.json({ message }, { status: 400 });
